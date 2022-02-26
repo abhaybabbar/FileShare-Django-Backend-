8fb62c68a0348e02d1e2f46882126d04d565f17c
Abhay Babbar <babbarabhay1852@gmail.com>
2022-02-26 23:10:39 +0530

    .

--- fileshare/settings.py
+++ fileshare/settings.py
@@ -124,10 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
