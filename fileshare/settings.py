b905f9db9571593d36e93c0ce6b81f272eaab269
Abhay Babbar <babbarabhay1852@gmail.com>
2022-02-26 23:33:51 +0530

    .

--- fileshare/settings.py
+++ fileshare/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import 
+import os
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
